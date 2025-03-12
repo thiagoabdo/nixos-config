91adebf19e4932511414c283aafcc33afb203a93
thiago abdo <tj.xd@hotmail.com>
2025-03-12 15:23:16 -0300

    try cowsay
    Signed-off-by: thiago abdo <tj.xd@hotmail.com>

--- home/tjota/home.nix
+++ home/tjota/home.nix
@@ -22,6 +22,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.cowsay
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
