d8f4f246f18caed6bee1ae166d18b38f2839c17e
thiago abdo <tj.xd@hotmail.com>
2025-03-12 15:28:03 -0300

    try homemanager
    Signed-off-by: thiago abdo <tj.xd@hotmail.com>

--- hosts/explorer-vm/default.nix
+++ hosts/explorer-vm/default.nix
@@ -36,6 +36,6 @@
 
 # networking.hostName = "nixos"; # Define your hostname.
 
-{
-  imports = [ ../common ./configuration.nix ];
+{ inputs } : {
+  imports = [ ../common ./configuration.nix inputs.home-manager.nixosModules.home-manager];
 }
